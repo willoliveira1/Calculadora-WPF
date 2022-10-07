77799f755fc42e65b41aef151687599913a508c3
Will Oliveira <57854155+willoliveira1@users.noreply.github.com>
2022-10-07 11:51:11 -0300

    Inclusão de funcionalidade para teclado.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,20 +21,12 @@ namespace Calculadora_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        //myKeyPressClass myKeyPressHandler = new myKeyPressClass();
-        //public Form1()
-        //{
-        //    InitializeComponent();
-
-        //    textBox1.KeyPress += new KeyPressEventHandler(myKeyPressHandler.myKeyCounter);
-        //}
-
         public MainWindow()
         {
             InitializeComponent();
         }
         
-        string[] operators = { "+", "-", "*", "/" };
+        //string[] operators = { "+", "-", "*", "/" };
         string value1 = String.Empty;
         double value2 = 0;
         string mathOperator = String.Empty;
@@ -120,6 +112,121 @@ namespace Calculadora_WPF
             value2 = 0;
             mathOperator = String.Empty;
             Input.Text = String.Empty;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.NumPad0 || e.Key == Key.D0)
+            {
+                Input.Text += "0";
+            }
+            if (e.Key == Key.NumPad1 || e.Key == Key.D1)
+            {
+                Input.Text += "1";
+            }
+            if (e.Key == Key.NumPad2 || e.Key == Key.D2)
+            {
+                Input.Text += "2";
+            }
+            if (e.Key == Key.NumPad3 || e.Key == Key.D3)
+            {
+                Input.Text += "3";
+            }
+            if (e.Key == Key.NumPad4 || e.Key == Key.D4)
+            {
+                Input.Text += "4";
+            }
+            if (e.Key == Key.NumPad5 || e.Key == Key.D5)
+            {
+                Input.Text += "5";
+            }
+            if (e.Key == Key.NumPad6 || e.Key == Key.D6)
+            {
+                Input.Text += "6";
+            }
+            if (e.Key == Key.NumPad7 || e.Key == Key.D7)
+            {
+                Input.Text += "7";
+            }
+            if (e.Key == Key.NumPad8 || e.Key == Key.D8)
+            {
+                Input.Text += "8";
+            }
+            if (e.Key == Key.NumPad9 || e.Key == Key.D9)
+            {
+                Input.Text += "9";
+            }
+
+            if (e.Key == Key.OemComma || e.Key == Key.Decimal)
+            {
+                Input.Text += ",";
+            }
+
+            if (e.Key == Key.Divide)
+            {
+                value1 = Input.Text.ToString();
+                mathOperator = "/";
+                Input.Text = String.Empty;
+            }
+            if (e.Key == Key.Add)
+            {
+                value1 = Input.Text.ToString();
+                mathOperator = "+";
+                Input.Text = String.Empty;
+            }
+            if (e.Key == Key.OemMinus || e.Key == Key.Subtract)
+            {
+                value1 = Input.Text.ToString();
+                mathOperator = "-";
+                Input.Text = String.Empty;
+            }
+            if (e.Key == Key.Multiply)
+            {
+                value1 = Input.Text.ToString();
+                mathOperator = "*";
+                Input.Text = String.Empty;
+            }
+
+            if (e.Key == Key.OemPlus || e.Key == Key.Return) // =
+            {
+                if (value1.Count() > 0)
+                {
+                    double val1 = double.Parse(value1);
+                    value2 = double.Parse(Input.Text);
+
+                    switch (mathOperator)
+                    {
+                        case "+":
+                            Input.Text = (val1 + value2).ToString();
+                            break;
+                        case "-":
+                            Input.Text = (val1 - value2).ToString();
+                            break;
+                        case "*":
+                            Input.Text = (val1 * value2).ToString();
+                            break;
+                        case "/":
+                            Input.Text = (val1 / value2).ToString();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            if (e.Key == Key.C || e.Key == Key.Escape)
+            {
+                Clear();
+            }
+
+            if (e.Key == Key.Back)
+            {
+                if(Input.Text.Count() > 0)
+                {
+                    var temp = Input.Text;
+                    Input.Text = temp.Remove(temp.Length - 1);
+                }
+            }
         }
     }
 }
